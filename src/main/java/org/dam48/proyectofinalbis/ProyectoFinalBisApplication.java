9958ae3d9c15b7df9c479197a2138486a9f5a286
dam23 <blancarodrifernan@gmail.com>
2025-03-10 02:36:56 +0100

    10/03/2025

--- src/main/java/org/dam48/proyectofinalbis/ProyectoFinalBisApplication.java
+++ src/main/java/org/dam48/proyectofinalbis/ProyectoFinalBisApplication.java
@@ -10,18 +10,18 @@ public class ProyectoFinalBisApplication {
     public static void main(String[] args) {
 
         // Establecer el túnel SSH
-        try {
-            SQLDatabaseManager.connectSSH();
-        } catch (Exception e) {
-            System.err.println("No se pudo abrir el túnel SSH: " + e.getMessage());
-            System.exit(1); // Detiene la aplicación si falla la conexión SSH
-        }
+//        try {
+//            SQLDatabaseManager.connectSSH();
+//        } catch (Exception e) {
+//            System.err.println("No se pudo abrir el túnel SSH: " + e.getMessage());
+//            System.exit(1); // Detiene la aplicación si falla la conexión SSH
+//        }
 
         // Iniciar Spring Boot después de abrir el túnel
         SpringApplication.run(ProyectoFinalBisApplication.class, args);
 
         // Cerrar el túnel cuando Spring Boot se detenga
-        Runtime.getRuntime().addShutdownHook(new Thread(SQLDatabaseManager::disconnectSSH));
+        //Runtime.getRuntime().addShutdownHook(new Thread(SQLDatabaseManager::disconnectSSH));
 
     }
 }
